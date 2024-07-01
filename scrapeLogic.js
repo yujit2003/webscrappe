604b7268ad15f9020524cc7bdb8d08dcac5af175
Yujit Yadav <97657345+yujit2003@users.noreply.github.com>
2024-07-01 09:12:36 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
